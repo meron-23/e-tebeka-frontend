27832afd53fe88605fd8deb45933a58d564afc8e
meron-23 <meronmuluye75@gmail.com>
2026-04-19 10:17:10 -0700

    Update client.ts

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api/v1";
+const API_BASE_URL = "https://e-tebeka-backend-5.onrender.com/api/v1" || "http://localhost:8000/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
